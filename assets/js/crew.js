5a1309712fbfeef2308f750c4e08f7994d8a802a
leersmathieu <leers.mathieu@gmail.com>
2018-05-25 15:35:07 +0200

    v0.2 and updating code for teamworking

--- assets/js/crew.js
+++ assets/js/crew.js
@@ -2,6 +2,10 @@ const unlockHouston = () => {
     houston.div.style.visibility = 'visible'
 }
 
+const unlockWick = () => {
+    wick.div.style.visibility = 'visible'
+}
+
 ///////////////////////////////////CREW/////////////////////////////////
 
 let recruitDallas = () => {
@@ -9,26 +13,24 @@ let recruitDallas = () => {
     if (money > dallas.prix) {
 
         if (dallas.level == 1) {
+
             crimeMultiplier = crimeMultiplier + 1
             moneyMultiplier = moneyMultiplier + 0.5
 
         }
         
-
         moneyMultiplier = moneyMultiplier + 0.5
 
         money = money - dallas.prix
         money = Math.round(money * 100) / 100 //(debug)
 
         dallas.prix = dallas.prix + 300
-
         dallas.level++
 
         statsValue[2].innerText = 'Compte en banque : ' + money + ' $'
         statsValue[3].innerText = 'Multiplicateur (crime) : ' + crimeMultiplier
         statsValue[4].innerText = 'Multiplicateur (money) : ' + moneyMultiplier
         dallas.div.innerText = `Upgrade ${dallas.name} : ${dallas.prix} $ \n Level : ${dallas.level}`
-
 
     } else {
         alert('Solde insuffisant')
@@ -39,12 +41,15 @@ let recruitDallas = () => {
 let recruitChains = () => {
 
     if (money > chains.prix) {
+
         if (chains.level == 1) {
+
             crimeMultiplier = crimeMultiplier + 1
             moneyMultiplier = moneyMultiplier + 0.5
 
         }
         if (chains.clickAuto == undefined) {
+
             chains.clickAuto = window.setInterval(autoClickBase, 2000)
 
         }
@@ -54,15 +59,12 @@ let recruitChains = () => {
         money = Math.round(money * 100) / 100
 
         chains.prix = chains.prix + 400
-
         chains.level++
 
         statsValue[2].innerText = 'Compte en banque : ' + money + ' $'
         statsValue[3].innerText = 'Multiplicateur (crime) : ' + crimeMultiplier
         statsValue[4].innerText = 'Multiplicateur (money) : ' + moneyMultiplier
         chains.div.innerText = `Upgrade ${chains.name} : ${chains.prix} $ \n Level : ${chains.level}`
-
-
 
     } else {
         alert('Solde insuffisant')
@@ -73,12 +75,15 @@ let recruitChains = () => {
 let recruitHoxton = () => {
 
     if (money > hoxton.prix) {
+
         if (hoxton.level == 1) {
+
             crimeMultiplier = crimeMultiplier + 1
             moneyMultiplier = moneyMultiplier + 0.5
 
         }
         if (hoxton.clickAuto == undefined) {
+
             hoxton.clickAuto = window.setInterval(autoClickBase, 1500)
 
         }
@@ -88,15 +93,12 @@ let recruitHoxton = () => {
         money = Math.round(money * 100) / 100
 
         hoxton.prix = hoxton.prix + 500
-
         hoxton.level++
 
         statsValue[2].innerText = 'Compte en banque : ' + money + ' $'
         statsValue[3].innerText = 'Multiplicateur (crime) : ' + crimeMultiplier
         statsValue[4].innerText = 'Multiplicateur (money) : ' + moneyMultiplier
         hoxton.div.innerText = `Upgrade ${hoxton.name} : ${hoxton.prix} $ \n Level : ${hoxton.level}`
-
-
 
     } else {
         alert('Solde insuffisant')
@@ -107,12 +109,15 @@ let recruitHoxton = () => {
 let recruitWolf = () => {
 
     if (money > wolf.prix) {
+
         if (wolf.level == 1) {
+
             crimeMultiplier = crimeMultiplier + 1
             moneyMultiplier = moneyMultiplier + 0.5
 
         }
         if (wolf.clickAuto == undefined) {
+
             wolf.clickAuto = window.setInterval(autoClickBase, 1000)
 
         }
@@ -122,15 +127,12 @@ let recruitWolf = () => {
         money = Math.round(money * 100) / 100
 
         wolf.prix += 600
-
         wolf.level++
 
         statsValue[2].innerText = 'Compte en banque : ' + money + ' $'
         statsValue[3].innerText = 'Multiplicateur (crime) : ' + crimeMultiplier
         statsValue[4].innerText = 'Multiplicateur (money) : ' + moneyMultiplier
         wolf.div.innerText = `Upgrade ${wolf.name} : ${wolf.prix} $ \n Level : ${wolf.level}`
-
-
 
     } else {
         alert('Solde insuffisant')
@@ -141,7 +143,9 @@ let recruitWolf = () => {
 let recruitHouston = () => {
 
     if (money > houston.prix) {
+
         if (houston.level == 1) {
+
             crimeMultiplier = crimeMultiplier + 1
 
         }
@@ -152,7 +156,6 @@ let recruitHouston = () => {
         money = Math.round(money * 100) / 100
 
         houston.prix += 5000
-
         houston.level++
 
         statsValue[2].innerText = 'Compte en banque : ' + money + ' $'
@@ -160,7 +163,34 @@ let recruitHouston = () => {
         statsValue[4].innerText = 'Multiplicateur (money) : ' + moneyMultiplier
         houston.div.innerText = `Upgrade ${houston.name} : ${houston.prix} $ \n Level : ${houston.level}`
 
+    } else {
+        alert('Solde insuffisant')
+    }
 
+}
+
+let recruitWick = () => {
+
+    if (money > wick.prix) {
+
+        if (wick.level == 1) {
+
+            crimeMultiplier = crimeMultiplier + 1
+
+        }
+
+        moneyMultiplier = moneyMultiplier + (moneyMultiplier * 0.5)
+
+        money = money - wick.prix
+        money = Math.round(money * 100) / 100
+
+        wick.prix += 100000
+        wick.level++
+
+        statsValue[2].innerText = 'Compte en banque : ' + money + ' $'
+        statsValue[3].innerText = 'Multiplicateur (crime) : ' + crimeMultiplier
+        statsValue[4].innerText = 'Multiplicateur (money) : ' + moneyMultiplier
+        wick.div.innerText = `Upgrade ${wick.name} : ${wick.prix} $ \n Level : ${wick.level}`
 
     } else {
         alert('Solde insuffisant')
